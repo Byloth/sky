8e0ce3664a8539993fe32e997a31004f26ef667d
Bilotta Matteo <m.bilotta@yuss.it>
2016-04-27 08:14:50 +0200

    Rinominati i metodi della classe 'net.byloth.engine.utils.SunTimes'.

--- engine/src/main/java/net/byloth/engine/utils/SunTimes.java
+++ engine/src/main/java/net/byloth/engine/utils/SunTimes.java
@@ -15,48 +15,48 @@ public class SunTimes
     static final private int SUNSET = -1;
 
     static final private double OFFICIAL = 90.8333d;
-    static final private double CIVIL = 96.8333d;
-    static final private double NAUTICAL = 102.8333d;
-    static final private double ASTRONOMICAL = 108.8333d;
+    static final private double CIVIL = 96;
+    static final private double NAUTICAL = 102;
+    static final private double ASTRONOMICAL = 108;
 
-    static private double meanObliquity(double julianCenturies)
+    static private double getMeanObliquity(double julianCenturies)
     {
         double seconds = Maths.polynomialFunction(new double[] { -0.001813, 0.00059, -46.815, 21.448 }, julianCenturies);
 
         return (23 + ((26 + (seconds / 60)) / 60));
     }
 
-    static private double obliquityCorrection(double julianCenturies)
+    static private double getObliquityCorrection(double julianCenturies)
     {
-        double meanObliquity = meanObliquity(julianCenturies);
+        double meanObliquity = getMeanObliquity(julianCenturies);
         double omega = 125.04d - (1934.136d * julianCenturies);
 
         return (meanObliquity + (0.00256d * Maths.cosine(omega, Maths.DEGREES)));
     }
 
-    static private double meanLongitude(double julianCenturies)
+    static private double getMeanLongitude(double julianCenturies)
     {
         double meanLongitude = Maths.polynomialFunction(new double[] { 0.0003032d, 36000.76983d , 280.46646d }, julianCenturies);
 
         return Maths.adjustInRange(meanLongitude, Maths.MAX_DEGREES);
     }
 
-    static private double eccentricityEarthOrbit(double julianCenturies)
+    static private double getEccentricityEarthOrbit(double julianCenturies)
     {
         return Maths.polynomialFunction(new double[] { 0.0000001267d, -0.000042037d, 0.016708634d }, julianCenturies);
     }
 
-    static private double meanAnomaly(double julianCenturies)
+    static private double getMeanAnomaly(double julianCenturies)
     {
         return Maths.polynomialFunction(new double[] { -(1.0d / 300000), -0.0001603d, 35999.05034d, 357.52772d }, julianCenturies);
     }
 
-    static private double equationOfTime(double julianCenturies)
+    static private double getEquationOfTime(double julianCenturies)
     {
-        double epsilon = obliquityCorrection(julianCenturies);
-        double meanLong = meanLongitude(julianCenturies);
-        double eccentricity = eccentricityEarthOrbit(julianCenturies);
-        double meanAnomaly = meanAnomaly(julianCenturies);
+        double epsilon = getObliquityCorrection(julianCenturies);
+        double meanLong = getMeanLongitude(julianCenturies);
+        double eccentricity = getEccentricityEarthOrbit(julianCenturies);
+        double meanAnomaly = getMeanAnomaly(julianCenturies);
 
         double y = Math.pow(Maths.tangent(epsilon / 2, Maths.DEGREES), 2);
 
@@ -76,9 +76,9 @@ public class SunTimes
         return (Math.toDegrees(equationOfTime) * 4);
     }
 
-    static private double equationOfCenter(double julianCenturies)
+    static private double getEquationOfCenter(double julianCenturies)
     {
-        double meanAnomaly = meanAnomaly(julianCenturies);
+        double meanAnomaly = getMeanAnomaly(julianCenturies);
 
         meanAnomaly = Math.toRadians(meanAnomaly);
 
@@ -91,46 +91,36 @@ public class SunTimes
                 (meanAnomalySine3 * 0.000289d));
     }
 
-    static private double trueLongitude(double julianCenturies)
+    static private double getTrueLongitude(double julianCenturies)
     {
-        double meanLong = meanLongitude(julianCenturies);
-        double equationOfCenter = equationOfCenter(julianCenturies);
+        double meanLong = getMeanLongitude(julianCenturies);
+        double equationOfCenter = getEquationOfCenter(julianCenturies);
 
         return (meanLong + equationOfCenter);
     }
 
-    static private double apparentLongitude(double julianCenturies)
+    static private double getApparentLongitude(double julianCenturies)
     {
-        double trueLong = trueLongitude(julianCenturies);
+        double trueLong = getTrueLongitude(julianCenturies);
 
         double omega = 125.04d - (julianCenturies * 1934.136d);
 
         return ((trueLong - 0.00569d) - (Maths.sine(omega, Maths.DEGREES) * 0.00478d));
     }
 
-    static private double rightAscension(double epsilon, double lambda)
+    static private double getDeclination(double julianCenturies)
     {
-        epsilon = Math.toRadians(epsilon);
-        lambda = Math.toRadians(lambda);
-
-        double rightAscension = Maths.arcTangent(Math.cos(lambda), Math.cos(epsilon) * Math.sin(lambda), Maths.DEGREES);
-
-        return Maths.adjustInRange(rightAscension, Maths.MAX_DEGREES);
-    }
-
-    static private double declination(double julianCenturies)
-    {
-        double epsilon = obliquityCorrection(julianCenturies);
-        double lambda = apparentLongitude(julianCenturies);
+        double epsilon = getObliquityCorrection(julianCenturies);
+        double lambda = getApparentLongitude(julianCenturies);
 
         double julianCenturiesSine = Maths.sine(epsilon, Maths.DEGREES) * Maths.sine(lambda, Maths.DEGREES);
 
         return Math.asin(julianCenturiesSine);
     }
 
-    static private double hourAngle(double julianCenturies, double latitude, int hourAngleType, double twilightType)
+    static private double getHourAngle(double julianCenturies, double latitude, int hourAngleType, double twilightType)
     {
-        double declination = declination(julianCenturies);
+        double declination = getDeclination(julianCenturies);
 
         latitude = Math.toRadians(latitude);
 
@@ -140,17 +130,17 @@ public class SunTimes
         return (Maths.arcCosine(sunriseHourAngle, Maths.DEGREES) * hourAngleType);
     }
 
-    static private double time(double julianDay, double julianCenturies, double equationOfTime, double latitude, double longitude, double timeZoneOffset, int hourAngleType, double twilightType)
+    static private double getTime(double julianDay, double julianCenturies, double equationOfTime, double latitude, double longitude, double timeZoneOffset, int hourAngleType, double twilightType)
     {
-        double hourAngle = hourAngle(julianCenturies, latitude, hourAngleType, twilightType);
+        double hourAngle = getHourAngle(julianCenturies, latitude, hourAngleType, twilightType);
 
         double delta = longitude + hourAngle;
 
         julianCenturies = (720 - (delta * 4)) - equationOfTime;
         julianCenturies = JulianDate.toJulianCenturies(julianDay + (julianCenturies / 1440));
 
-        equationOfTime = equationOfTime(julianCenturies);
-        hourAngle = hourAngle(julianCenturies, latitude, hourAngleType, twilightType);
+        equationOfTime = getEquationOfTime(julianCenturies);
+        hourAngle = getHourAngle(julianCenturies, latitude, hourAngleType, twilightType);
 
         delta = longitude + hourAngle;
 
@@ -174,30 +164,30 @@ public class SunTimes
     private void initializeNoonTime(double longitude, double timeZoneOffset)
     {
         double julianCenturies = JulianDate.toJulianCenturies(julianDate.getJulianDay() + (longitude / Maths.MAX_DEGREES));
-        double equationOfTime = equationOfTime(julianCenturies);
+        double equationOfTime = getEquationOfTime(julianCenturies);
 
         julianCenturies = (720 - (longitude * 4)) - equationOfTime;
 
         julianCenturies = JulianDate.toJulianCenturies(julianDate.getJulianDay() + (julianCenturies / 1440));
-        equationOfTime = equationOfTime(julianCenturies);
+        equationOfTime = getEquationOfTime(julianCenturies);
 
         noonTime = Maths.adjustInRange(((720 - (longitude * 4)) - equationOfTime) + timeZoneOffset, 1440);
     }
 
     private void initializeSunriseTimes(double julianCenturies, double equationOfTime, double latitude, double longitude, double timeZoneOffset)
     {
-        officialDawnTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, OFFICIAL);
-        civilDawnTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, CIVIL);
-        nauticalDawnTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, NAUTICAL);
-        astronomicalDawnTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, ASTRONOMICAL);
+        officialDawnTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, OFFICIAL);
+        civilDawnTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, CIVIL);
+        nauticalDawnTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, NAUTICAL);
+        astronomicalDawnTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, DAWN, ASTRONOMICAL);
     }
 
     private void initializeSunsetTimes(double julianCenturies, double equationOfTime, double latitude, double longitude, double timeZoneOffset)
     {
-        officialSunsetTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, OFFICIAL);
-        civilSunsetTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, CIVIL);
-        nauticalSunsetTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, NAUTICAL);
-        astronomicalSunsetTime = time(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, ASTRONOMICAL);
+        officialSunsetTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, OFFICIAL);
+        civilSunsetTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, CIVIL);
+        nauticalSunsetTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, NAUTICAL);
+        astronomicalSunsetTime = getTime(julianDate.getJulianDay(), julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset, SUNSET, ASTRONOMICAL);
     }
 
     public SunTimes(Location location)
@@ -216,7 +206,7 @@ public class SunTimes
         initializeNoonTime(longitude, timeZoneOffset);
 
         double julianCenturies = JulianDate.toJulianCenturies(julianDate.getJulianDay());
-        double equationOfTime = equationOfTime(julianCenturies);
+        double equationOfTime = getEquationOfTime(julianCenturies);
 
         initializeSunriseTimes(julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset);
         initializeSunsetTimes(julianCenturies, equationOfTime, latitude, longitude, timeZoneOffset);
